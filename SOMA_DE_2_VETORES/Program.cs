56c95f97c0601cd5224a7fae1313bdec46336d4a
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-08 15:38:23 -0300

    primeiro commit

---
+++ SOMA_DE_2_VETORES/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SOMA_DE_2_VETORES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            double [] y = new double[n];
+            double [] x = new double[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = double.Parse(s[i]);
+            }
+
+            s = Console.ReadLine().Split(' ');
+            for (int i = 0;i < n; i++)
+            {
+                y[i] = double.Parse(s[i]);
+            }
+
+            double[] C = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                C[i] = y[i] + x[i];
+                Console.Write(C[i] + " ");
+            }
+            
+        }
+    }
+}
